38c751ad4db97360d5ef50ed2e9f33ed2eed66c6
Frame0505 <yaroslav.rudenko.2000@bk.ru>
2021-01-13 16:44:56 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://cdn.icon-icons.com/icons2/1826/PNG/512/4202092logomovienetflixsocialsocialmediavideo-115596_115705.png"/>
+          </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return (
+    <div className="content">
+        <img src="https://img1.akspic.ru/fit/22572-nacionalnyj_park-pustynya-banff-nacionalnyj_park_grand_titon-nacionalnyj_park_glejsher-x350.jpg" />
+        <div>
+            ava + description
+            </div>
+        <div>
+            My posts
+              <div>
+                New post
+              </div>
+            <div>
+                post 1
+              </div>
+            <div>
+                post 2
+              </div>
+        </div>
+    </div>
+    )
+}
+
+export default Profile
